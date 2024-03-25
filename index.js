912273757e6c077bdaf93b6c275ad353c94141bb
luciogayoso <luciogayoso@gmail.com>
2024-03-25 17:41:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function name(params) {
-    console.log("hola mundo");
+function ejercicio5() {
+
+    let factor = prompt("Ingrese un numero para calcular el factorial");
+
+    let producto = 1;
+    for (let i = 1; i <= factor; i++) {
+        producto = producto * i;
+    }
+
+    console.log("El factorial de "+ factor + " es: "+producto);
 }
